6dab1d4473029d155a0b9b6951d92fcf9424e605
Priyamvada Shah <priyamvadashahlko@gmail.com>
2024-06-27 16:59:54 +0530

    upd toggle

--- src/components/ToggleBtn/index.jsx
+++ src/components/ToggleBtn/index.jsx
@@ -1,13 +1,7 @@
 import './toggle.css';
 const ToggleBtn = ({ imgSrc, handleToggleMode, textColor}) => (
-        <button
-          id="tgl"
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold border border-blue-500 hover:border-transparent rounded w-1/2"
-          onClick={handleToggleMode}
-          style ={{color: textColor}}
-        >
-          <img src={imgSrc} alt="Toggle Mode" style={{  height: '2.55rem', width: '8rem', borderRadius: '10%' }} />
-        </button>
+
+          <img id="tgl" src={imgSrc} onClick={handleToggleMode}alt="Toggle Mode" style={{  height: '2.55rem', width: '2.5rem', borderRadius: '10%' }} />
   );
   
   export default ToggleBtn;
